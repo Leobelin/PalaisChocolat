5d02e1bf64e5ca51c5ad8799fc6c206472c21b62
Leobelin <166522471+Leobelin@users.noreply.github.com>
2024-05-02 16:38:25 +0200

    footer

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ fetch("chocolatier.json")
     .then(data => {
         // Appelle la fonction addgateaux() avec les données JSON en entrée
         addgateaux(data.realisations);
+        addavis(data.temoignages);
+        addbene(data.listeBenefficesClients);
     });
 
 // Cette fonction prend les données JSON en entrée et effectue une action sur celles-ci
@@ -31,24 +33,51 @@ function addgateaux(tableauRealisations) {
         `
     })
 }
-addavis(data.temoignages);
 
 // Cette fonction prend les données JSON en entrée et effectue une action sur celles-ci
 function addavis(tableauTemoignages) {
 
-// Affiche les données dans la console du navigateur
+    // Affiche les données dans la console du navigateur
 
-tableauTemoignages.forEach(temoignages => {
+    tableauTemoignages.forEach(temoignages => {
 
 
 
-    document.querySelector("#temoignages").innerHTML +=
-        `
+        document.querySelector("#temoignages").innerHTML +=
+            `
     <div class="avis">
     <h3>${temoignages.prenom} </h3>
     <p>${temoignages.typePrestation}</p>
     <p>${temoignages.commentaire}</p>
-    <p>${temoignages.note}</p>
+    <p> note: ${temoignages.note}</p>
     </div>
     `})
 }
+// Cette fonction prend les données JSON en entrée et effectue une action sur celles-ci
+function addbene(tableauListeBenefficesClients) {
+
+    // Affiche les données dans la console du navigateur
+
+    tableauListeBenefficesClients.forEach(listeBenefficesClients => {
+
+
+
+        document.querySelector("#listeBenefficesClients").innerHTML +=
+            `
+        <div class="bene">
+        <h3>${listeBenefficesClients} </h3>
+        </div>
+        `})
+}
+
+
+var map = L.map('map').setView([47.22575885581474, -1.5517634805755953], 13);
+
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	maxZoom: 17,
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+}).addTo(map);
+
+L.marker([47.22575885581474, -1.5517634805755953]).addTo(map)
+    .bindPopup("<h1>Le Palais Du Chocolat</h1><p>35 Rue des Gobelets Nantes 44200</p>")
+    .openPopup();5
