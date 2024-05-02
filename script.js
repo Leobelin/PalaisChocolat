9e15a8ad1b519cb7ee7426726a5da45a455d2841
Leobelin <166522471+Leobelin@users.noreply.github.com>
2024-05-02 10:54:38 +0200

    02/05/2024

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-// Envoie une requête GET à l'URL de l'api pour récupérer des données
+// Envoie une requête GET à l'URL de  pour récupérer des données
 fetch("chocolatier.json")
     // Une fois que la réponse est reçue
     .then(rep => {
         // transforme la reponse obtenu en JSON 
         return rep.json();
     })
-    // Une fois que les données JSON sont extraites avec succès, jeles transmet à une fonction pour l'utiliser plus tard
+    // Une fois que les données JSON sont extraites avec succès, je les transmet à une fonction pour l'utiliser plus tard
     .then(data => {
-        // Appelle la fonction addProducts() avec les données JSON en entrée
+        // Appelle la fonction addgateaux() avec les données JSON en entrée
         addgateaux(data.realisations);
     });
 
 // Cette fonction prend les données JSON en entrée et effectue une action sur celles-ci
 function addgateaux(tableauRealisations) {
-    
-    // Affiche les données dans la console du navigateur
-    
-    tableauRealisations.forEach(realisations => {
-       
-    
 
-        document.querySelector("#content").innerHTML+=
-        `
+    // Affiche les données dans la console du navigateur
+
+    tableauRealisations.forEach(realisations => {
+
+
+
+        document.querySelector("#content").innerHTML +=
+            `
         <div class="carte">
         <h2>${realisations.nom} </h2>
         <p>${realisations.description}</p>
@@ -29,4 +29,26 @@ function addgateaux(tableauRealisations) {
         <a href="" class="bouton">Acheté</a>
         </div>
         `
-    })}
+    })
+}
+addavis(data.temoignages);
+
+// Cette fonction prend les données JSON en entrée et effectue une action sur celles-ci
+function addavis(tableauTemoignages) {
+
+// Affiche les données dans la console du navigateur
+
+tableauTemoignages.forEach(temoignages => {
+
+
+
+    document.querySelector("#temoignages").innerHTML +=
+        `
+    <div class="avis">
+    <h3>${temoignages.prenom} </h3>
+    <p>${temoignages.typePrestation}</p>
+    <p>${temoignages.commentaire}</p>
+    <p>${temoignages.note}</p>
+    </div>
+    `})
+}
